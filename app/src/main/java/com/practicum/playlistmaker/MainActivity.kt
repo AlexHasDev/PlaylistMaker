aba70289942f23ff12634cec1e777d7e1ae51492
AlexHas <alexhas.dev@gmail.com>
2023-03-06 18:31:51 +0500

    Playlist maker 0.01

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.practicum.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val search = findViewById<Button>(R.id.search)
+        val media = findViewById<Button>(R.id.media)
+        val settings = findViewById<Button>(R.id.settings)
+
+        search.setOnClickListener{val searchIntent = Intent(this, SearchActivity::class.java)
+        startActivity(searchIntent)}
+
+        media.setOnClickListener{val mediaIntent = Intent(this, MediaActivity::class.java)
+        startActivity(mediaIntent)}
+
+        settings.setOnClickListener{val settingsIntent = Intent(this, SettingsActivity::class.java)
+        startActivity(settingsIntent)}
     }
 }
